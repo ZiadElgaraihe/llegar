6ef9eb20ab79356220dd27cf6778e366f45f48fc
ZiadElgaraihe <ziadelgaraihe301@gmail.com>
2023-08-07 13:03:39 +0300

    add reset password service to setup service locator

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -2,30 +2,42 @@ import 'package:get_it/get_it.dart';
 import 'package:llegar/core/user_model_cubit/user_model_cubit.dart';
 import 'package:llegar/core/utils/helpers/dio_helper.dart';
 import 'package:llegar/features/auth/data/services/log_in_service.dart';
+import 'package:llegar/features/auth/data/services/reset_password_service.dart';
 import 'package:llegar/features/auth/data/services/sign_up_service.dart';
 import 'package:llegar/features/auth/presentation/view_model/sign_up_cubit/sign_up_cubit.dart';
 
 final GetIt getIt = GetIt.instance;
 
 void setupServiceLocator() {
+  //dio helper
   getIt.registerSingleton<DioHelper>(
     DioHelper(),
   );
+  //log in service
   getIt.registerSingleton<LogInService>(
     LogInService(
       dioHelper: getIt.get<DioHelper>(),
     ),
   );
+  //sign up service
   getIt.registerSingleton<SignUpService>(
     SignUpService(
       dioHelper: getIt.get<DioHelper>(),
     ),
   );
+  //reset password service
+  getIt.registerSingleton<ResetPasswordService>(
+    ResetPasswordService(
+      dioHelper: getIt.get<DioHelper>(),
+    ),
+  );
+  //sign up cubit
   getIt.registerSingleton<SignUpCubit>(
     SignUpCubit(
       signUpService: getIt.get<SignUpService>(),
     ),
   );
+  //user model cubit
   getIt.registerSingleton<UserModelCubit>(
     UserModelCubit(),
   );
